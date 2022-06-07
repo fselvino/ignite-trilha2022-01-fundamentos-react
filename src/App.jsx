a29a4104b574231361f49743ef95ef68943ac841
fselvino <fernandoselvino@gmail.com>
2022-06-06 23:33:27 -0300

    Ate Propriedades do Post

---
+++ src/App.jsx
@@ -0,0 +1,80 @@
+import { Header } from './components/Header'
+import { Post } from './components/Post'
+import { Sidebar } from './components/Sidebar'
+
+
+import styles from './App.module.css'
+import './global.css'
+
+// author: { avatar_url: '', nome: '', role:'' }
+// publishedAt: Date
+// content:strig
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/fselvino.png',
+      name: 'Fernando Selvino',
+      role: 'CTO @ ServiceBI'
+
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala pessoal 👋' },
+      { type: 'paragraph', content: 'Finalmente finalizei meu novo site/portfólio. Foi um baita desafio criar todo o design e codar na unha, mas consegui 💪🏻' },
+      { type: 'paragraph', content: 'Acesse e deixe seu feedback 👉' },
+      { type: 'link', content: 'devonlane.design' },
+
+    ],
+    publishedAt: new Date('2022-05-03 20:00:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/maykbrito.png',
+      name: 'Mayk Brito',
+      role: 'Educator @Rocketseat'
+
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala pessoal 👋' },
+      { type: 'paragraph', content: 'Finalmente finalizei meu novo site/portfólio. Foi um baita desafio criar todo o design e codar na unha, mas consegui 💪🏻' },
+      { type: 'paragraph', content: 'Acesse e deixe seu feedback 👉' },
+      { type: 'link', content: 'devonlane.design' },
+
+    ],
+    publishedAt: new Date('2022-05-04 20:00:00')
+  },
+]
+
+
+export function App() {
+
+  return (
+    <>
+      <Header />
+      <div className={styles.wrapper}>
+
+        <Sidebar />
+        <main>
+      {
+        posts.map(post => {
+          return (
+          <Post
+            author={post.author}
+            content={post.content}
+            publishedAt={post.publishedAt}
+          />
+          )
+        })
+      }
+
+        </main>
+      </div>
+
+    </>
+  )
+}
+
+
+
---
+++ src/components/Comment.jsx
@@ -0,0 +1,41 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import styles from './Comment.module.css'
+
+import { Avatar } from './Avatar'
+
+export function Comment() {
+    return (
+        <div className={styles.comment}>
+            <Avatar 
+            src="https://github.com/diego3g.png"
+            hasBorder={false}
+            />
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Diego Fernandes</strong>
+                            <time
+                                title='11 de Maio às 08:13'
+                                dateTime='2022-05-11 05:13:00'
+                            >Cerca de 1h atras</time>
+                        </div>
+                        <button title='Deletar comentário'>
+                            <Trash
+                            size={24}/>
+                        </button>
+                        
+                    </header>
+                    <p>Muito bom dev, parabens</p>
+                </div>
+                <footer>
+                    <button>
+                        <ThumbsUp/>
+                        Aplaudir<span>20</span>
+                    </button>
+                </footer>
+
+            </div>
+        </div>
+    )
+}
